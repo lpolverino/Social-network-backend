3d2b1b9c20f8cd2f78e1b5ac488f810f1531516f
lpolverino <lorenz.polverino@gmail.com>
2024-02-10 16:45:25 -0300

    Added sing up route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const authentication = require("../authenticate")
 const passport = require("passport")
 const jwt = require("jsonwebtoken")
+const asyncHandler = require('express-async-handler')
+const { body, validationResult } = require("express-validator")
+const User = require("../model/user")
+const bcrypt = require('bcryptjs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -29,9 +33,50 @@ router.post('/login',(req,res,next) =>{
   })(req, res);
 })
 
-router.post('/singup',(req,res,next) =>{
-  res.json({message:"singup"})
-})
+router.post('/singup',
+  body("username")
+  .trim()
+  .isLength({min:1, max:100})
+  .withMessage("incorrect Length"),
+  body("password")
+  .trim()
+  .isLength({min:5})
+  .withMessage("password too short"),
+  body("email")
+  .trim()
+  .isLength({min:1})
+  .withMessage("email should not be empty")
+  .isEmail()
+  .withMessage("email bad formated"),
+  asyncHandler(async (req,res,next) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).send({ errors: result.array() });
+    }
+
+    const user = await User.findOne({user_name:req.body.username}).select("user_name").exec()
+
+    if(user !== null){
+      return res.status(400).send({errors:[{msg:"username already taken"}]})      
+    }
+
+    const newUser = await bcrypt.hash(req.body.password, 10)
+      .then(hashedPassword => {
+        return new User({
+          user_name:req.body.username,
+          password: hashedPassword,
+          email:req.body.email
+        })
+      })
+      .catch( e =>{
+        return res.status(500).json({errors:[{msg:"cannot save user"}]})
+      })
+      const savedUser = await newUser.save()
+ 
+    const token = jwt.sign({savedUser}, process.env.SECRET_P);
+    return res.json({token,user:savedUser._id});
+  })
+);
 
 router.get('/auth/github',
   passport.authenticate('github', { scope: [ 'user:email' ],session:false })
